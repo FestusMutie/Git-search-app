1d06f8030b4831b3b0e885ded061e0b78c4c93bb
FestusMutie <mutiefestus84@gmail.com>
2021-05-22 17:21:59 +0300

    add github profile card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubAppComponent } from './github-app/github-app.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './github-profile-card/github-profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubAppComponent,
-    GithubProfileComponent
+    GithubProfileComponent,
+    GithubProfileCardComponent
   ],
   imports: [
     BrowserModule,
